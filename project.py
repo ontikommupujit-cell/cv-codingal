224f7741a1d08066dd01e75ad88305417b0c67aa
pujit <ontikommupujit@gmail.com>
2026-08-24 13:35:10 -0400

    hi

---
+++ project.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+image = cv2.imread("fortnitephoto.jpg")
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+(h, w) = image.shape[:2]
+center = (w // 2, h // 2)
+M = cv2.getRotationMatrix2D(center, 180, 1.0)
+rotated_image = cv2.warpAffine(image, M, (w, h))
+rotated_rgb = cv2.cvtColor(rotated_image, cv2.COLOR_BGR2RGB)
+plt.imshow(rotated_rgb)
+plt.title('Rotated Image')
+plt.show()
+brightness_matrix = np.ones(image.shape, dtype="uint8") * 100
+brighter = cv2.add(image, brightness_matrix)
+brighter_rgb = cv2.cvtColor(brighter, cv2.COLOR_BGR2RGB)
+plt.imshow(brighter_rgb)
+plt.title('Brighter Image')
+plt.show()
+cropped = image[100:400, 100:400]
+cropped_rgb = cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB)
+plt.imshow(cropped_rgb)
+plt.title('Cropped Image')
+plt.show()
